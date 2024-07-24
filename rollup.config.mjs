639887c7c36c79e30df96f6be9f60c5d14156b7e
koladeolukoya <koladeolukoya@gmail.com>
2024-07-24 14:27:32 +0100

    feat: added typescript

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,11 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 import babel from "@rollup/plugin-babel";
 
 export default {
-  input: 'src/index.jsx',
+  input: 'src/index.tsx',
   output: [
     {
       file: 'dist/bundle.cjs.js',
@@ -21,6 +22,7 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
     babel({
       exclude: 'node_modules/**',
       babelHelpers: 'bundled',
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const HelloWorld = () => {
   return (
     <div>
-      Hello, World!
+      Hello, World
+      With Typescript!
     </div>
   );
 };
